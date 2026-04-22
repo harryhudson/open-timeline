25f3728380364dd8af9e86e526f760c4b74f5417
Harry Hudson <60789276+harryhudson@users.noreply.github.com>
2026-04-22 13:06:39 +0100

    Database CLI working

--- bins/src/bin/db.rs
+++ bins/src/bin/db.rs
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 //!
-//! *Part of the wider OpenTimeline project*
+//! Simple database management CLI
 //!
-//! The OpenTimeline website ([www.open-timeline.org](www.open-timeline.org))
+//! ## Usage
+//!
+//! - `cargo r --bin db`
+//! - `cargo r --bin db -- create  --database /path/to/sqlite/database`
+//! - `cargo r --bin db -- backup  --database /path/to/sqlite/database --json /path/to/json/data/`
+//! - `cargo r --bin db -- merge   --database /path/to/sqlite/database --json /path/to/json/data/`
+//! - `cargo r --bin db -- restore --database /path/to/sqlite/database --json /path/to/json/data/`
+//! - `cargo r --bin db -- stats   --database /path/to/sqlite/database`
 //!
 
 use clap::{CommandFactory, Parser, ValueEnum, builder::PossibleValue};
-use open_timeline_crud::OpenTimelineDatabase;
-use sqlx::{Connection, SqliteConnection};
+use open_timeline_crud::{DatabaseRowCount, OpenTimelineDatabase};
 use std::path::PathBuf;
 
-/// OpenTimeline entry point
-///
-/// One of:
-/// - Backup the database
-/// - Restore the database
-/// - Serve the website and API
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
 
     // Check the options
@@ -27,63 +27,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Valid
         //----------------------------------------------------------------------
         (Command::Create, database_path, _) => {
-            match OpenTimelineDatabase::setup_at_path(database_path).await {
-                Ok(()) => println!("Success"),
+            let result: anyhow::Result<()> = async move {
+                OpenTimelineDatabase::setup_at_path(database_path).await?;
+                Ok(())
+            }
+            .await;
+            helper_print_result(result);
+        }
+        (Command::Backup, database_path, Some(data_dir)) => {
+            let op = BackupMergeRestore::Backup;
+            helper_database_operation(op, database_path.clone(), data_dir.clone()).await;
+        }
+        (Command::Merge, database_path, Some(data_dir)) => {
+            let op = BackupMergeRestore::Merge;
+            helper_database_operation(op, database_path.clone(), data_dir.clone()).await;
+        }
+        (Command::Restore, database_path, Some(data_dir)) => {
+            let op = BackupMergeRestore::Restore;
+            helper_database_operation(op, database_path.clone(), data_dir.clone()).await;
+        }
+        (Command::Stats, database_path, _) => {
+            let result: anyhow::Result<DatabaseRowCount> = async move {
+                let db_url = OpenTimelineDatabase::url_from_path(&database_path);
+                let read_only = false;
+                let pool = OpenTimelineDatabase::sqlite_pool_from_url(&db_url, read_only).await?;
+                let mut transaction = pool.begin().await?;
+                Ok(DatabaseRowCount::all(&mut transaction).await?)
+            }
+            .await;
+            match result {
+                Ok(count) => {
+                    println!("Success");
+                    println!(" - Entities          = {}", count.entities);
+                    println!(" - Entity tags       = {}", count.entity_tags);
+                    println!(" - Timelines         = {}", count.timelines);
+                    println!(" - Subtimelines      = {}", count.subtimelines);
+                    println!(" - Timeline entities = {}", count.timeline_entities);
+                    println!(" - Timeline tags     = {}", count.timeline_tags);
+                }
                 Err(error) => {
                     eprintln!("Error: {error}");
                     std::process::exit(1);
                 }
-            }
-        }
-        (Command::Backup, _database_path, Some(_json)) => {
-            todo!()
-        }
-        (Command::Restore, database_path, Some(json)) => {
-            // Generate database URL
-            let db_url = OpenTimelineDatabase::url_from_path(database_path);
-
-            // Open database connection
-            let mut connection = match SqliteConnection::connect(&db_url).await {
-                Ok(connection) => connection,
-                Err(error) => {
-                    eprintln!("Error connecting to database: {error}");
-                    std::process::exit(1);
-                }
             };
-
-            // Begin database transaction
-            let mut transaction: sqlx::Transaction<'_, sqlx::Sqlite> =
-                match connection.begin().await {
-                    Ok(transaction) => transaction,
-                    Err(error) => {
-                        eprintln!("Error starting transaction: {error}");
-                        std::process::exit(1);
-                    }
-                };
-
-            // Restore the database
-            match OpenTimelineDatabase::restore_from_dir(&mut transaction, json.to_owned()).await {
-                Ok(()) => (),
-                Err(error) => {
-                    eprintln!("Error restoring database: {error}");
-                    std::process::exit(1);
-                }
-            }
-
-            // Commit the transaction
-            match transaction.commit().await {
-                Ok(()) => println!("Sucessfully restored database"),
-                Err(error) => {
-                    eprintln!("Error committing transaction: {error}");
-                    std::process::exit(1);
-                }
-            }
-        }
-        (Command::Merge, _database, Some(_json)) => {
-            todo!()
-        }
-        (Command::Stats, _database, _) => {
-            todo!()
         }
         //----------------------------------------------------------------------
         // Invalid
@@ -96,6 +82,57 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+/// Database operation
+enum BackupMergeRestore {
+    Backup,
+    Merge,
+    Restore,
+}
+
+/// Helper to carry out database operation
+async fn helper_database_operation(
+    op: BackupMergeRestore,
+    database_path: PathBuf,
+    data_dir: PathBuf,
+) {
+    let result: anyhow::Result<()> = async move {
+        // Connect to database
+        let db_url = OpenTimelineDatabase::url_from_path(&database_path);
+        let read_only = false;
+        let pool = OpenTimelineDatabase::sqlite_pool_from_url(&db_url, read_only).await?;
+        let mut transaction = pool.begin().await?;
+
+        // Run operation
+        match op {
+            BackupMergeRestore::Backup => {
+                OpenTimelineDatabase::backup_to_dir(&mut transaction, data_dir).await?
+            }
+            BackupMergeRestore::Merge => {
+                OpenTimelineDatabase::merge_from_dir(&mut transaction, data_dir).await?
+            }
+            BackupMergeRestore::Restore => {
+                OpenTimelineDatabase::restore_from_dir(&mut transaction, data_dir).await?
+            }
+        }
+
+        // Commit changes
+        transaction.commit().await?;
+        Ok(())
+    }
+    .await;
+    helper_print_result(result);
+}
+
+fn helper_print_result(result: anyhow::Result<()>) {
+    match result {
+        Ok(()) => println!("Success"),
+        Err(error) => {
+            eprintln!("Error: {error}");
+            std::process::exit(1);
+        }
+    };
 }
 
 /// OpenTimeline CLI args using [clap]
